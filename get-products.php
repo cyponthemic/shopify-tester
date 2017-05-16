3856d6ea0429cffff88b4416a2b8d07f59db399f
Alex CHAVET <alex.chavet@gmail.com>
2017-05-17 06:46:58 +1000

    Working

--- get-products.php
+++ get-products.php
@@ -6,9 +6,9 @@ use GuzzleHttp\Client;
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$db = new Mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASS'), getenv('MYSQL_DB')); 
+$db = new Mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASS'), getenv('MYSQL_DB'));
 
-$store = 'test-shop.myshopify.com'; 
+$store = 'driver-app.myshopify.com'; 
 $select = $db->query("SELECT access_token FROM installs WHERE store = '$store'");
 $user = $select->fetch_object();
 $access_token = $user->access_token;
@@ -16,7 +16,7 @@ $access_token = $user->access_token;
 $client = new Client();
 
 $response = $client->request(
-	'GET', 
+	'GET',
 	"https://{$store}/admin/products.json",
 	[
 		'query' => [
